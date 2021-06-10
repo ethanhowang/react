0acdeaf8c7623e1ed400972b3bf7aea6d8392a07
Ethan Wang <ethan_wang@berkeley.edu>
2021-06-10 11:09:21 -0700

    fix

--- my-website/src/index.js
+++ my-website/src/index.js
@@ -1,8 +1,21 @@
 import React from "react";
 import ReactDom from "react-dom";
 
+// stateless functional component
+// always return JSX
+// JSX rules
+// always return a component
+// use camelCase for html attribute
+// className instead of class
+// close every element
+// formatting
+
 function Greeting() {
-  return <h4>this is ethan and this is my first component</h4>;
+  return (
+    <div>
+      <h1>hello world</h1>
+    </div>
+  );
 }
 
 ReactDom.render(<Greeting />, document.getElementById("root"));
