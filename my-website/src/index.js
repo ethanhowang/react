3b28a007d932851be246cd579f198bc01d18b3a5
Ethan Wang <ethan_wang@berkeley.edu>
2021-06-21 15:23:16 -0700

    somebooks

--- my-website/src/index.js
+++ my-website/src/index.js
@@ -1,5 +1,7 @@
 import React from "react";
 import ReactDom from "react-dom";
+// CSS
+import "./index.css";
 
 // stateless functional component
 // always return JSX
@@ -10,12 +12,42 @@ import ReactDom from "react-dom";
 // close every element
 // formatting
 
-function Greeting() {
+// Nested Components, React Tools
+
+function BookList() {
   return (
-    <div>
-      <h1>hello world</h1>
-    </div>
+    <section className="booklist">
+      <Book />
+      <Book />
+      <Book />
+      <Book />
+      <Book />
+    </section>
   );
 }
 
-ReactDom.render(<Greeting />, document.getElementById("root"));
+const Book = () => {
+  return (
+    <article className="book">
+      <Image></Image>
+      <Title></Title>
+      <Author></Author>
+    </article>
+  );
+};
+
+const Image = () => (
+  <img
+    src="https://images-na.ssl-images-amazon.com/images/I/81eB%2B7%2BCkUL._AC_UL200_SR200,200_.jpg"
+    alt=""
+  />
+);
+
+const Title = () => <h1>I Love You to the Moon and Back</h1>;
+const Author = () => (
+  <h4 style={{ color: "#617d98", fontSize: "0.75rem", marginTop: "0.25rem" }}>
+    Amelia Hepworth
+  </h4>
+);
+
+ReactDom.render(<BookList />, document.getElementById("root"));
